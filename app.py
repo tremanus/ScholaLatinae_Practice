850dd1f8a944293cbad959473a3191ec9c189571
tremanus <lbiyabrook@gmail.com>
2024-07-24 11:39:50 -0400

    back to 10

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ def get_leaderboard(quiztype):
         FROM results
         WHERE quiztype = %s
         ORDER BY score DESC
-        LIMIT 12
+        LIMIT 10
     ''', (quiztype,))
     rows = cursor.fetchall()
     cursor.close()
